bcf949e95b1fc2505eb85c8dc69e11f8dd532d36
Valentin <valentine.karnaukhov@gmail.com>
2022-05-11 16:51:20 +0300

    Preloaded keycloak configuration

--- deploy/new_server.sh
+++ deploy/new_server.sh
@@ -1,6 +1,6 @@
-sudo apt update
-sudo apt install docker.io -y
-sudo apt install docker-compose -y
+apt update
+apt install docker.io -y
+apt install docker-compose -y
 
 HOST_IP=`wget -qO- ipecho.net/plain`
 export HOST_IP=${HOST_IP}
